964b4b1df8b49689387ae0485730653db88a2d78
Gods Will Khojee <ardiumwing@gmail.com>
2025-08-16 18:12:35 +0530

    committed

---
+++ app.js
@@ -0,0 +1,31 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+    setTimeout(()=>
+    {
+        logoSpan.forEach((span,idx) => 
+          {
+            setTimeout(()=>{
+              span.classList.add('active')
+          }, (idx+1)*500); // 700
+      });
+
+      setTimeout(()=>{
+        logoSpan.forEach((span,idx)=>
+          {
+            setTimeout(()=>{
+              span.classList.remove('active');
+              span.classList.add('fade');
+            }, (idx+1)*100) //700
+        })
+      },2400); //2700
+      setTimeout(() => {
+        intro.style.top= '-100vh'
+      },3000); //6100
+  })
+})
+
